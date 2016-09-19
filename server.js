0b1d32bc3976a8ed3b6c586c7f8ba479612abe21
Rudri <rudrikansara@gmail.com>
2016-09-19 15:47:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 var articles = {
     'article-one': {
-        title:'Article Three | Rudri Kansara',
+        title:'Article One | Rudri Kansara',
         heading:'Article One',
         date:'September 19,2016',
         content:`
@@ -29,7 +29,7 @@ var articles = {
                     <p>
                 `},
     'article-three' : {title:'Article Three | Rudri Kansara',
-        heading:'Article One',
+        heading:'Article Three',
         date:'September 21,2016',
         content:`
                     <p>
