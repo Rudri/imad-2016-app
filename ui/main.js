064a13290ba57765582cf16c792a9e3f290014c6
Rudri <rudrikansara@gmail.com>
2016-09-21 15:48:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ element.innerHTML = 'new value';
 
 //Move the image 
 
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 
 img.onclick = function () {
     log.style.marginLeft="100px";
