c6a29ba6385aaab11fb8ac14111ab2472d415ae4
Rudri <rudrikansara@gmail.com>
2016-09-23 13:53:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,8 @@ button.onclick = function () {
 // Submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
-var submit = document.getElementById('Submit_btn');
-submit.onclick = function(){
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
   //Make a request to the server and send the name
   
   //Capture the list of names and render it as a list
